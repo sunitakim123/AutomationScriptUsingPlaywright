914bee626b013ebec9714e3394fb3ab9eb2378f5
sunita <sunitakim123@gmail.com>
2024-12-31 12:38:31 +0530

    maven-surfire plugin updated in pom

--- src/test/java/com/SignUP.java
+++ src/test/java/com/SignUP.java
@@ -24,7 +24,7 @@ public class SignUP extends BaseTest {
 
 	@Test(priority=1)
 	public void Sign_Up_with_Valid_Credentials() throws IOException, InterruptedException {
-		//page.navigate(properties.getProperty("rootUrl"));
+		page.navigate(properties.getProperty("rootUrl"));
 		page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName("Sign Up Sign Up For Free")).click();
 		//page.navigate("https://awspf.com/signup");
 		page.getByPlaceholder("Email Address").fill(generateRandomEmail());
